a93b187dc70e780f5b377b4a0fab5ec83c4a1b2c
Saifalikhan21 <64728822+Saifalikhan21@users.noreply.github.com>
2020-06-14 20:08:49 +0500

    Add files via upload

---
+++ javascript assignment/app.js
@@ -0,0 +1,383 @@
+
+// let i=10;
+// document.write('the value of a is :'+i+'</br>');
+
+// i++;
+// document.write('the value of ++a is :'+i+'</br>');
+// document.write('the value of a is :'+i+'</br>');
+
+
+
+// document.write('the value of a++ is :'+i+'</br>');
+// ++i;
+// document.write('the value of a is :'+i+'</br>');
+
+// --i;
+// document.write('the value of a is :'+i+'</br>');
+// document.write('the value of a is :'+i+'</br>');
+// document.write('the value of a is :'+i+'</br>');
+
+// i--;
+
+// document.write('the value of a is :'+i+'</br>');
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result);
+
+
+
+// var name = window. prompt("Enter your name: ");
+// alert("welcome " + name);
+
+// var num = window. prompt("Enter table: ");
+// for(i=1;i<=10;i++)
+// {
+
+// num1=num*i;
+// document.write(num +'x'+ i +'='+ num1+'</br>');
+
+
+//}
+
+// document.write()
+// var course1 = window. prompt("Enter course 1: ");
+// var course2 = window. prompt("Enter course 2: ");
+// var course3 = window. prompt("Enter course 3: ");
+// var t_marks=100;
+
+// var marks1 = window. prompt("Enter marks 1: ");
+// var marks2 = window. prompt("Enter marks 2: ");
+// var marks3 = window. prompt("Enter marks 3: ");
+
+
+
+                // ASSIGN NO 9-11
+
+
+
+
+//  var city = window. prompt("Enter CITY NAME: ");
+// if(city== 'karachi' || city== 'KARACHI')
+// {
+// document.write('welcome to city of lights');
+
+// }
+
+// var gender = window. prompt("enter gender: ");
+// if(gender== 'male' || city== 'MALE')
+// {
+// document.write('good morning sir');
+
+
+// }
+// if(gender== 'female' || city== 'FEMALE')
+// {
+// document.write('good morning Maam');
+// }
+
+
+
+// var car;
+//  var fuel = window. prompt("Enter fuel: ");
+// if(fuel < 0.25){
+
+
+//     document.write("Please refill the fuel in your car");
+// }
+
+
+
+// var s_num=5;
+// var number = window. prompt("Enter number: ");
+// if (number == s_num)
+// {
+//    document.write("bingo you are correct!!!");
+   
+// }
+// else
+// if(number  == 6 || number == 7)
+// {
+
+//     document.write("You are close enough!!!");
+   
+// }
+
+
+//  var number = window. prompt("Enter number: ");
+// if(number%3 ==0)
+// {
+//    document.write("divisible");
+
+
+// }
+// else{
+
+
+//     document.write("not divisible");
+
+// }
+
+
+
+// var number = window. prompt("Enter number: ");
+//  if(number%2 ==0)
+//  {
+//    document.write("Even");
+
+
+//  }
+//  else{
+
+
+//    document.write("odd");
+
+//  }
+
+// var t1= 40;
+// var t2= 30;
+// var t3= 20;
+// var t4= 10;
+
+// var temp = window. prompt("Enter temp: ");
+// if(temp>t1)
+// {
+//    document.write("It is too hot outside.");
+
+// }
+
+// if(temp>t2)
+// {
+//     document.write("The Weather today is Normal");
+// }
+
+// if(temp>t3)
+// {
+//     document.write("today, weather is cool");
+// }
+// if(temp>t4)
+// {
+//     document.write("OMG! Today’s weather is so Cool");
+
+// }
+
+            // ASSIGN 12-13
+
+
+
+
+//  var num1 = window. prompt("Enter number: ");
+//  var num2 = window. prompt("Enter number 2: ");
+// if(num1 > num2)
+// {
+
+//      document.write("number 1 is greater");
+
+// }
+// if(num2 > num1)
+// {
+
+//      document.write("number 2 is greater");
+
+// }
+
+// if(num2 == num1)
+// {
+
+//      document.write("equal");
+
+// }
+
+
+
+
+
+//   var num1 = window. prompt("Enter number: ");
+
+//  if(num1 > 0)
+// / {
+
+// //      document.write("positive);
+
+// // }
+// // if(num1 < 0 )
+// // {
+
+// //      document.write("less than");
+
+// // }
+
+// // if(num1 == 0)
+// // {
+
+// //      document.write("equal");
+
+// // }
+
+
+
+// var pwd= 'saylani'
+//   var pwd1 = window. prompt("Enter number: ");
+// if(pwd1==pwd)
+// {
+
+//     document.write("correct");
+
+// }
+// else{
+
+//     document.write("incorrect password");
+
+
+// }
+
+// var hour = 13;
+// if (hour < 18) {
+//      document.write("good day");
+
+
+// }else{
+// greeting = "Good evening";
+// }
+
+
+// time=1900;
+// if(time >= 0000 && time< 1200)
+// {
+//   document.write("good morning");
+
+// }
+
+
+// if(time >= 1200 && time< 1700)
+// {
+//   document.write("good afternoon");
+
+// }
+// if(time >= 1700 && time< 2100)
+// {
+//   document.write("good evening");
+
+// }
+// if(time >= 2100 && time< 2300)
+// {
+//   document.write("good night");
+
+// }
+
+
+
+// var arrayName = [];
+// var arrayName = [];
+// var cars = ["Saab", "Volvo", "BMW"];
+// var nums1 = [1,2,3,4];
+// var nums2 = [1,2,3,4,"Saab", "Volvo", "BMW"];
+
+// var nums3 = ['SSC', 'HSC', 'BCS','BS', 'BCOM', 'MS', 'M.Phil', 'PhD'];
+// for(i=0;i<1;i++)
+// {
+    
+//     document.write(nums3);
+// }
+
+// let name=['michael', 'john', 'tony'];
+// let marks=['320', '230', '480'];
+
+// for(i=0;i<=2;i++)
+// {
+//     perc=(marks[i]/500)*100;
+
+// document.write(' '+name[i]+' is '+marks[i]+'. Percentage: '+perc);
+
+// }
+
+// let color=['blue', 'green','white','purple'];
+// color.unshift('red');
+// document.write(color);
+
+
+// let color=['blue', 'green','white','purple'];
+//  color.push('maroon');
+// document.write(color);
+
+// let color=['blue', 'green','white','purple'];
+//  color.unshift('maroon', 'black');
+// document.write(color);
+
+//  let color=['blue', 'green','white','purple'];
+//  color.shift();
+//  document.write(color);
+
+
+ 
+//  let color=['blue', 'green','white','purple'];
+//  color.shift();
+//  document.write(color);
+
+//  let color=['blue', 'green','white','purple'];
+//  color.pop();
+//  document.write(color);
+
+
+//  let color=['blue', 'green','white','purple'];
+//   color.splice(2,1);
+//  document.write(color);
+
+// task2
+
+// var points = [40, 100, 1, 5, 25, 10];
+//   document.write(points);
+
+//   points.sort();
+//   document.write(points);
+
+
+
+// var city=['karachi','lahore', 'islamabad', 'quetta']
+// city.splice(4,3);
+// document.write(city);
+
+// var arr = ['this', ' is ', ' my ', 'cat'];
+// arr.join();
+// document.write(arr);
+
+// left
+
+
+
+
+
+
+
+
+// chapters 17-20
+
+// let activities = [
+//     [0, 1,2,3,'<br>'],
+//     [1, 0,1,2,'<br>'],
+//     [2, 1,0,1,'<br>']
+// ];
+
+// document.write(activities);
+
+
+
+// for (var input = 1; input <= 10; input++) {
+//     console.log(input);
+//    }
+
+//   var n = window. prompt("table: ");
+
+// for (i = 1; i <= 15; ++i) {
+//     document.write( n, i, n * i);
+// }
+
+let num1;
+let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+document.write(fruits+'<br>')
+for(i=0;i<=3;i++)
+{
+num1.findIndex(fruits[i]);
+document.write(num1);
+}
